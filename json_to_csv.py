0fcf971b864e08cab64c394bcae4bdd09d1f8ea2
fhalamos <felipealamos@gmail.com>
2021-03-05 11:14:27 -0300

    Adding new rows to pandas df

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,5 +1,5 @@
 import ijson
-
+import pandas as pd
 
 def parse_json_2(json_filename):
 
@@ -8,32 +8,40 @@ def parse_json_2(json_filename):
         print(objects)
 
 def parse_json(json_filename):
+
+    df = pd.DataFrame()
+
     with open(json_filename, 'rb') as input_file:
         # load json iteratively
         parser = ijson.parse(input_file)
-        valid_keys = ['CompletionDate', 'SubmissionDate']
-        counter=0
+        #valid_keys = ['CompletionDate', 'SubmissionDate']
+        rows_counter=0
         row = {}
         for prefix, event, value in parser:
             #Check if prefix we are reading is related to one of the expected keys
-            if len(prefix.split('.'))>1 and prefix.split('.')[1] in valid_keys:
+            if len(prefix.split('.'))>1:# and prefix.split('.')[1] in valid_keys:
                 key = prefix.split('.')[1]
-                #Check if we are starting a new row
+
+                #If key is CompletionDate, we are starting a new row, add it to df and clear row dict
                 if key == 'CompletionDate':
-                    #If row has content, save it to csv
+
                     if row != {}:
-                        print(row)#Save row to csv
-                    
+                        df = df.append(row, ignore_index=True)
+
+                        if rows_counter % 50 == 0:
+                            print(df)
+
+                        rows_counter +=1
+
                     #Clear row
                     row = {}
 
                 #Add key value to row
                 row[key]=value
 
-                counter +=1
-             
-            #if counter >1000:
-            #    break
+
+
+
 
 if __name__ == '__main__':
     parse_json('mask_data_wide_complete.json')
