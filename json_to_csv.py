a51cc2a3376889efda957d3e5a54bbe5ba162485
fhalamos <felipealamos@gmail.com>
2021-03-08 12:11:10 -0300

    Finish csv creationg with all keys

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,5 +1,7 @@
 import ijson
 import pandas as pd
+import os
+import sys
 
 def parse_json_2(json_filename):
 
@@ -7,41 +9,111 @@ def parse_json_2(json_filename):
         objects = ijson.items(input_file)
         print(objects)
 
-def parse_json(json_filename):
+def create_empty_dict(dict_keys):
+    dict = {}
+    for key in dict_keys:
+        dict[key] = ""
+    return dict
 
-    df = pd.DataFrame()
+def get_all_keys(json_filename):
+
+    all_keys = []
 
     with open(json_filename, 'rb') as input_file:
         # load json iteratively
         parser = ijson.parse(input_file)
-        #valid_keys = ['CompletionDate', 'SubmissionDate']
-        rows_counter=0
-        row = {}
         for prefix, event, value in parser:
-            #Check if prefix we are reading is related to one of the expected keys
-            if len(prefix.split('.'))>1:# and prefix.split('.')[1] in valid_keys:
+            #Check if prefix we are reading is a json key
+            if len(prefix.split('.'))>1:
                 key = prefix.split('.')[1]
 
-                #If key is CompletionDate, we are starting a new row, add it to df and clear row dict
-                if key == 'CompletionDate':
+                if key not in all_keys:
+                    all_keys.append(key)
+                    print(key)
+    return all_keys
 
-                    if row != {}:
-                        df = df.append(row, ignore_index=True)
 
-                        if rows_counter % 50 == 0:
-                            print(df)
+def add_rows_to_csv(rows, csv_file, sorted_columns, include_header):
 
-                        rows_counter +=1
+    #Add rows to a df
+    df = pd.DataFrame(rows)
 
-                    #Clear row
-                    row = {}
+    #Sort the df by json_keys, so that when appending to csv they all respect the same order
+    df = df[sorted(sorted_columns)]
+
+    #Append df to csv, add header only in first insertion
+    df.to_csv(csv_file, mode='a', index=False, header= include_header)
+
+    print(f'Appended {len(rows)} rows')
+
+
+
+def parse_json_to_csv_with_keys(json_filename, json_keys):
+
+    df = pd.DataFrame()
+
+    OUTPUT_FILE = json_filename.split('.')[0]+'.csv'
+    #If output file exists, delete it
+    if os.path.exists(OUTPUT_FILE):
+        print(f'Removing old version of {OUTPUT_FILE}')
+        os.remove(OUTPUT_FILE)
+
+    with open(json_filename, 'rb') as input_file:
+        # load json iteratively
+        parser = ijson.parse(input_file)
+
+        rows_counter=0
+
+        row = create_empty_dict(json_keys)
+        EMPTY_ROW = create_empty_dict(json_keys)
+
+        temp_list_rows = []
+
+        for prefix, event, value in parser:
+            #Check if prefix we are reading is a json key
+            if len(prefix.split('.'))>1:
+                key = prefix.split('.')[1]
+
+                #First lets check we are not starting a new row. If we are, we should save the previous one and start a new one
+                if key == 'CompletionDate' and row != EMPTY_ROW:
+                    temp_list_rows.append(row)
+                    rows_counter +=1
+                    row = create_empty_dict(json_keys)
+
+                    n_rows_to_append_at_a_time = 1000
+                    #Every 1000 rows, we will clear the temp_list_rows and append to the csv
+                    if rows_counter != 0 and rows_counter % n_rows_to_append_at_a_time == 0:
+
+                        add_rows_to_csv(rows = temp_list_rows,
+                                        csv_file = OUTPUT_FILE,
+                                        sorted_columns = json_keys,
+                                        include_header = rows_counter == n_rows_to_append_at_a_time)
+
+                        #Empty temp list of temp_rows
+                        temp_list_rows = []
+
+                        print(f'Total rows appended: {rows_counter}')
+                        print('%%%%%%%%%%')
+
 
                 #Add key value to row
                 row[key]=value
 
+        #Once finished, there might still be rows in temp_list_rows that we need to add to the .csv
+        add_rows_to_csv(rows = temp_list_rows,
+                        csv_file = OUTPUT_FILE,
+                        sorted_columns = json_keys,
+                        include_header = False)
 
+        print(f'Total rows appended: {rows_counter}')
+        print('%%%%%%%%%%')
 
-
+def parse_json_to_csv(json_file):
+    json_keys = get_all_keys(json_file)
+    print(f'Got json_keys, its {len(json_keys)} of them!')
+    parse_json_to_csv_with_keys(json_file, json_keys)
 
 if __name__ == '__main__':
-    parse_json('mask_data_wide_complete.json')
+    json_file = sys.argv[1]
+    # json_file = 'mask_data_wide_complete.json'
+    parse_json_to_csv(json_file)
