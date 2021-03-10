923e6ed52f343a08f3251811249b2190525da090
fhalamos <felipealamos@gmail.com>
2021-03-10 17:18:30 -0300

    Adding time prints

--- data_processing_pipeline.py
+++ data_processing_pipeline.py
@@ -1,11 +1,19 @@
 import surveycto_data_downloader
 import file_parser
 import sys
+from datetime import datetime
 
 def run_pipeline(start_day_timespam, server_name, form_id, username, password, survey_entries_path_destination, media_path_destination):
 
     #1. Download main .json
     # survey_entries_file_name = 'X:\\Box Sync\\MASK Test folder\\bdmaskrct-maskrct_phone_followup-1615320826.json'
+
+
+
+
+    now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    print(f"Starting download_survey_entries at {now}")
+
     survey_entries_file_name = surveycto_data_downloader.download_survey_entries(
         start_day_timespam=start_day_timespam,
         server_name=server_name,
@@ -19,11 +27,15 @@ def run_pipeline(start_day_timespam, server_name, form_id, username, password, s
     if survey_entries_file_name:
 
         #2. Transform main .json to .csv
+        now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        print(f"Starting parse_json_to_csv at {now}")
         file_parser.parse_json_to_csv(survey_entries_file_name)
 
 
         #3. Download attachments
         attachment_columns = ['text_audit', 'audio_audit']
+        now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        print(f"Starting download_attachments at {now}")
         surveycto_data_downloader.download_attachments(
             json_file = survey_entries_file_name,
             attachment_columns = attachment_columns,
