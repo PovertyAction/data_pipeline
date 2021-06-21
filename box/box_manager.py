1178dfbbc9dfd07b07fd986773268cb817b97e5f
Ubuntu <ubuntu@ip-172-31-33-60.us-east-2.compute.internal>
2021-06-21 23:36:37 +0000

    Fixed authentication with JWT

--- box/box_manager.py
+++ box/box_manager.py
@@ -12,10 +12,12 @@ from boxsdk import Client, OAuth2, JWTAuth
 def get_list_files(auth_method, box_folder_id):
 
     client = get_box_client(auth_method)
+    print(client)
 
     list_files = []
+    
     try:
-        items = client.folder(folder_id=box_folder_id).get_items()
+        items = client.folder(folder_id=str(box_folder_id)).get_items()
     except Exception as e:
         print("An exception occurred")
         print(e)
@@ -28,8 +30,6 @@ def get_list_files(auth_method, box_folder_id):
 def upload_file(auth_method, box_folder_id, file_path):
 
     client = get_box_client(auth_method)
-
-    print('e')
 
     try:
         box_folder = client.folder(folder_id=box_folder_id)
@@ -55,13 +55,19 @@ def get_box_client(authentication_method):
           access_token = box_credentials.get_developer_token()
         )
         client = Client(oauth)
+        return client
     elif authentication_method == 'jwt':
+        print('jwt client running')
         jwt_config_file_path = box_credentials.get_jwt_config_file_path()
         print(jwt_config_file_path)
         config = JWTAuth.from_settings_file(jwt_config_file_path)
         client = Client(config)
 
-    return client
+        #When auth with jwt, client is a service user. We need to get access to ipa box account with access to folders
+        #Reference: https://github.com/box/box-python-sdk/blob/main/docs/usage/authentication.md#server-auth-with-jwt
+        service_user = client.user(box_credentials.get_ipa_box_account_user_id())
+        return client.as_user(service_user)
+
 
 if __name__ == '__main__':
     function = sys.argv[1]
