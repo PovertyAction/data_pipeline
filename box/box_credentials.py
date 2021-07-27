e76f30770dd4507d93831c0beeb9299a4bed76db
fhalamos <felipealamos@gmail.com>
2021-07-26 17:06:25 -0700

    box credentials template file

---
+++ box/box_credentials.py
@@ -0,0 +1,12 @@
+def get_client_id():
+    return 'your_client_id'
+def get_client_secret():
+    return 'your_client_secret'
+def get_developer_token():
+    return 'your_developer_token'
+
+def get_jwt_config_file_name():
+    return 'your_jwt_config_file_name.json'
+
+def get_ipa_box_account_user_id():
+    return 'your_ipa_box_account_user_id'
