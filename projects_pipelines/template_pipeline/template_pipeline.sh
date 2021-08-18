ba0736ad740968d8e4847ff56381ab3d66403f5b
fhalamos <felipealamos@gmail.com>
2021-08-18 07:07:48 -0700

    splitting pipeline in methods for multiple forms to run

--- projects_pipelines/template_pipeline/template_pipeline.sh
+++ projects_pipelines/template_pipeline/template_pipeline.sh
@@ -5,21 +5,15 @@
 How to run:
 
 ./template_pipeline.sh \
---server bdmaskrct \
---download_wide_csv False \
---download_wide_json True \
---transform_json_to_csv True \
---form_id mask_monitoring_form_bn \
+--server migrationsurvey \
+--form_ids migration_midline_IP;migration_bc_IP;hh_survey_IP;migration_midline_tracking \
+--columns_with_attachments "text_audit,audio_hh_consent,audio_indiv_consent,audio_hh_head;text_audit;audio_hh_consent,audio_hh_head;text_audit" \
 --start_timestamp 0 \
 --username "researchsupport@poverty-action.org" \
---password "pass!" \
---box_path "./box_path_simulation/" \
---box_folder_id 139653613903 \
---s3_bucket mask-monitoring-project \
---columns_with_attachments "colA,colB" \
---media_box_path "./box_path_simulation/media" \
---media_box_folder_id 139653613903 \
---server_key_file_id "824680196213"
+--password "researchsupport_password" \
+--box_folder_id "142713774640" \
+--media_box_folder_id "142713042079" \
+--server_key_file_id "844416889094"
 
 Note: Not all inputs are necessary
 Mandatory inputs: server, form_id, start_timestamp, username, password, box_path or box_folder_id
@@ -28,20 +22,14 @@ Mandatory inputs: server, form_id, start_timestamp, username, password, box_path
 
 #Get user paramenters
 while [ $# -gt 0 ]; do
-   if [[ $1 == *"--"* ]]; then
-        param="${1/--/}"
-        declare $param="$2"
-   fi
+  if [[ $1 == *"--"* ]]; then
+    param="${1/--/}"
+    declare $param="$2"
+  fi
   shift
 done
 
 #> Pending: Should do some arguments validations here, like checking certain arguments are not empy and that others are valid
-
-#Build local folder for file
-outputs_folder="data/${server}/${form_id}"
-mkdir -p ${outputs_folder}
-echo "Will save outputs in: ${outputs_folder}"
-echo ''
 
 #Download surveycto server key
 if ! [ -z "${server_key_file_id}" ];
@@ -53,19 +41,13 @@ else
   server_key=''
 fi
 
-#1st argument is csv or json
+#1st argument is form_id
 download_survey_entries()
 {
-  format=$1
+  form_id=$1
 
   #Build url to download survey entries
-  if [ "$format" == "json" ];
-  then
-    url="https://${server}.surveycto.com/api/v2/forms/data/wide/json/${form_id}?date=${start_timestamp}"
-  elif [ "$format" == "csv" ];
-  then
-    url="https://${server}.surveycto.com/api/v1/forms/data/wide/csv/${form_id}?date=${start_timestamp}"
-  fi
+  url="https://${server}.surveycto.com/api/v2/forms/data/wide/json/${form_id}?date=${start_timestamp}"
   echo "Url to call: ${url}"
   echo ''
 
@@ -103,76 +85,88 @@ download_survey_entries()
   fi
 }
 
-#Download survey entries
-FILES_DOWNLOADED=""
-if [ "$download_wide_csv" == "True" ];
-then
-    download_survey_entries csv $server_key
-    FILES_DOWNLOADED=$(echo "$file_path")
-fi
+upload_files()
+{
 
-if [ "$download_wide_json" == "True" ];
-then
-    download_survey_entries json $server_key
+  files_to_upload=$1
 
-    #Add new file path to FILES_DOWNLOADED LIST
-    if [ "$FILES_DOWNLOADED" == "" ];
+  #Upload every output file to box drive, box cloud or aws
+  for file in ${files_to_upload}
+  do
+    #Copy file to box_path if box_path is not empty
+    if ! [ -z "${box_path}" ];
     then
-      FILES_DOWNLOADED=$(echo "$file_path")
-    else
-      FILES_DOWNLOADED+=$(echo " $file_path")
-    fi
-
-    #Transform to .csv.
-    if [ "$transform_json_to_csv" == "True" ];
-    then
-      parsed_csv_file_path="$(echo "$file_path" | cut -f 1 -d '.')_parsed.csv"
-      python3 ../../files_transformations/json_to_csv_parser.py "${file_path}" "${parsed_csv_file_path}"
-      echo "Parsed csv created"
-      echo "${parsed_csv_file_path}"
-      FILES_DOWNLOADED+=$(echo " $parsed_csv_file_path")
+      cp "${file}" "${box_path}/."
+      echo "${file} pushed to ${box_path} folder"
       echo ''
     fi
-fi
-echo 'Survey entries downloaded'
-echo "$FILES_DOWNLOADED"
 
-#Upload every output file to box drive, box cloud or aws
-for FILE in ${FILES_DOWNLOADED}
-do
-  #Copy file to box_path if box_path is not empty
-  if ! [ -z "${box_path}" ];
+    #Copy file to box
+    if ! [ -z "${box_folder_id}" ];
+    then
+      python3 ../../box/upload_to_box.py "${box_folder_id}" "${file}"
+      echo "${file} pushed to ${box_folder_id} in box.com"
+      echo ''
+    fi
+
+    #Copy file to aws
+    if ! [ -z "${s3_bucket}" ];
+    then
+      python3 ../../aws/upload_to_s3.py "${file}" "${s3_bucket}"
+      echo "${file} pushed to aws ${s3_bucket} bucket"
+      echo ''
+    fi
+  done
+}
+
+download_upload_attachments()
+{
+  columns_with_attachments=$1
+
+  #Download attachments (also to box path, box directly or aws)
+  if ! [ -z "${columns_with_attachments}" ];
   then
-    cp "${FILE}" "${box_path}/."
-    echo "${FILE} pushed to ${box_path} folder"
+
+    #Create key where to save media files in s3 bucket
+    s3_bucket_path_media="${outputs_folder}/media"
+
+    python3 ../../surveycto/download_upload_attachments.py --survey_file "${file_path}" --attachment_columns "${columns_with_attachments}" --username "${username}" --password "${password}" --encryption_key "${server_key}" --dest_path "${media_box_path}" --dest_box_id "${media_box_folder_id}" --s3_bucket "${s3_bucket}" --s3_bucket_path_media "${s3_bucket_path_media}"
+    echo "Attachements downloaded"
     echo ''
   fi
 
-  #Copy file to box
-  if ! [ -z "${box_folder_id}" ];
-  then
-    python3 ../../box/upload_to_box.py "${box_folder_id}" "${FILE}"
-    echo "${FILE} pushed to ${box_folder_id} in box.com"
-    echo ''
-  fi
+}
 
-  #Copy file to aws
-  if ! [ -z "${s3_bucket}" ];
-  then
-    python3 ../../aws/upload_to_s3.py "${FILE}" "${s3_bucket}"
-    echo "${FILE} pushed to aws ${s3_bucket} bucket"
-    echo ''
-  fi
-done
+download_upload_forms_data()
+{
 
-#Download attachments (also to box path, box directly or aws)
-if ! [ -z "${columns_with_attachments}" ];
-then
+  form_id=$1
+  columns_with_attachments=$3
 
-  #Create key where to save media files in s3 bucket
-  s3_bucket_path_media="${outputs_folder}/media"
-
-  python3 ../../surveycto/download_attachments.py --survey_file "${file_path}" --attachment_columns "${columns_with_attachments}" --username "${username}" --password "${password}" --encryption_key "${server_key}" --dest_path "${media_box_path}" --dest_box_id "${media_box_folder_id}" --s3_bucket "${s3_bucket}" --s3_bucket_path_media "${s3_bucket_path_media}"
-  echo "Attachements downloaded"
+  #Build local folder for file
+  outputs_folder="data/${server}/${form_id}"
+  mkdir -p ${outputs_folder}
+  echo "Will save outputs in: ${outputs_folder}"
   echo ''
-fi
+
+  #Download survey entries in json format
+  download_survey_entries $form_id
+
+  #Parse json to csv
+  parsed_csv_file_path="$(echo "$file_path" | cut -f 1 -d '.').csv"
+  python3 ../../files_transformations/json_to_csv_parser.py "${file_path}" "${parsed_csv_file_path}"
+  echo "Parsed csv created"
+  echo "${parsed_csv_file_path}"
+
+  #Upload files
+  files_to_upload=$(echo "$file_path $parsed_csv_file_path")
+  echo 'files_to_upload'
+  echo "$files_to_upload"
+  upload_files $files_to_upload
+
+  #Download attachments
+  download_attachments $columns_with_attachments
+
+}
+
+#Loop over each form and its att
